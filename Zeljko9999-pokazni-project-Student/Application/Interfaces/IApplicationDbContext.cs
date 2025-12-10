a1ce839cef2d511827daef20991104f91f5acfc5
Željko Kalajžić <zeljkokalajzic1@gmail.com>
2025-12-10 01:06:25 +0100

    HW03: set db connection

---
+++ Zeljko9999-pokazni-project-Student/Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Email> Emails { get; set; }
+    }
+}
---
+++ Zeljko9999-pokazni-project-Student/Infrastructure/Persistance/StudentDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistance
+{
+    public class StudentDbContext : DbContext, IApplicationDbContext
+    {
+        public StudentDbContext(DbContextOptions<StudentDbContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Email> Emails { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StudentDbContext).Assembly);
+        }
+    }
+}
